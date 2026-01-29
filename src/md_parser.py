bfd14630622e7c02e9cc32d3aeb38c6495438b28
ComplexAnimal <ElevatedAnimal@gmail.com>
2026-01-28 22:06:22 -0600

    Added md_parser.py and test_md_parser.py

---
+++ src/md_parser.py
@@ -0,0 +1,33 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+
+    new_nodes = []
+
+    for node in old_nodes:
+
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+
+        else:
+            split_nodes = node.text.split(delimiter)
+
+            if len(split_nodes) < 3 or len(split_nodes) % 2 == 0:
+                raise Exception("Error: The text is missing a closing delimiter")
+            
+            else:
+                node_group = []
+
+                for i in range(len(split_nodes)):
+
+                    if i % 2 == 0: # Should always be plain text
+                        new_node = TextNode(split_nodes[i], TextType.TEXT)
+                        node_group.append(new_node)
+
+                    else: # For all other text types
+                        new_node = TextNode(split_nodes[i], text_type)
+                        node_group.append(new_node)
+
+                new_nodes.extend(node_group)
+
+    return new_nodes
---
+++ src/test_md_parser.py
@@ -0,0 +1,60 @@
+import unittest
+
+from textnode import TextNode, TextType
+from md_parser import split_nodes_delimiter
+
+class TestMDParser(unittest.TestCase):
+
+    def test_only_non_text_tags(self): # nodes should remain unchanged
+        node1 = TextNode("Bold_text", TextType.BOLD)
+        node2 = TextNode("Italic_text", TextType.ITAL)
+        node3 = TextNode("Some_code", TextType.CODE)
+        nodes = [node1, node2, node3]
+        actual_result = split_nodes_delimiter(nodes, "_", TextType.ITAL)
+        self.assertEqual(actual_result, nodes)
+
+    def test_valid_italics(self):
+        node1 = TextNode("This text has _one italic_ section.", TextType.TEXT)
+        node2 = TextNode("_This text_ has two _italic sections._", TextType.TEXT)
+        nodes = [node1, node2]
+        expected_result = [
+            TextNode("This text has ", TextType.TEXT),
+            TextNode("one italic", TextType.ITAL),
+            TextNode(" section.", TextType.TEXT),
+            TextNode("", TextType.TEXT),
+            TextNode("This text", TextType.ITAL),
+            TextNode(" has two ", TextType.TEXT),
+            TextNode("italic sections.", TextType.ITAL),
+            TextNode("", TextType.TEXT),
+        ]
+        actual_result = split_nodes_delimiter(nodes, "_", TextType.ITAL)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_invalid_italics(self): # italics with missing closing delimiter
+        node = TextNode("This text is _missing a delimiter", TextType.TEXT)
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "_", TextType.ITAL)
+
+    def test_bold_text(self):
+        node = TextNode("This is **bold** text.", TextType.TEXT)
+        expected_result = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text.", TextType.TEXT),
+        ]
+        actual_result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_inline_code(self):
+        node = TextNode("This line `has code` in it.", TextType.TEXT)
+        expected_result = [
+            TextNode("This line ", TextType.TEXT),
+            TextNode("has code", TextType.CODE),
+            TextNode(" in it.", TextType.TEXT),
+        ]
+        actual_result = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(expected_result, actual_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
