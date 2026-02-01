de10c9372e24b37c6e4b7a007fc3945fedfc3fbd
ComplexAnimal <ElevatedAnimal@gmail.com>
2026-02-01 01:22:11 -0600

    Updated a bunch of stuff. Program can now generate multi-page static sites using files from a given directory

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
 import os
 import shutil
+from pathlib import Path
+
+from md_to_html import markdown_to_html_node
 
 def main():
+    # Copy static dir to public dir
     src = os.path.abspath("static")
     dst = os.path.abspath("public")
-    copy_dir(src, dst)
 
-def copy_dir(src, dst):
-    # Check for source and destination directories:
-    if not os.path.exists(src):
-        raise Exception("Error: source directory missing")
     if os.path.exists(dst):
         shutil.rmtree(dst)
-    os.mkdir(dst)
+
+    copy_dir(src, dst)
+
+    content_dir = Path("content")
+
+    for md_path in content_dir.rglob("*.md"):
+        relative = md_path.relative_to(content_dir)
+        relative_html = relative.with_suffix(".html")
+        dest_path = Path("public") / relative_html
+        generate_page(str(md_path), "template.html", str(dest_path))
+
+def copy_dir(src, dst):
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
     # Copy src contents to dst:
     contents = os.listdir(src)
     for item in contents:
@@ -35,5 +48,26 @@ def extract_title(markdown):
         raise Exception("Error: No h1 heading")
     else:
         return title
+
+def generate_page(from_path, template_path, dest_path):
+    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
+
+    with open(from_path) as file:
+        md_text = file.read()
+
+    with open(template_path) as file:
+        page = file.read()
+
+    title = extract_title(md_text)
+    node = markdown_to_html_node(md_text)
+    html = node.to_html()
+
+    page = page.replace("{{ Title }}", title)
+    page = page.replace("{{ Content }}", html)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    f = open(dest_path, "w")
+    f.write(page)
+    f.close()
 
 main()
