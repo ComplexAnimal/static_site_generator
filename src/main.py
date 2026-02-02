fdb5ea3bd4725738838037b69cf10ba838f8b9ec
ComplexAnimal <ElevatedAnimal@gmail.com>
2026-02-02 15:55:23 -0600

    Updated main.py to run generate_page("content/index.md", "template.html", "public/index.html") after copying static to public

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import os
-import shutil
 
 from copy_dir import copy_dir
+from generate_page import generate_page
 
 def main():
     src = os.path.abspath("static")
     dst = os.path.abspath("public")
 
     copy_dir(src, dst)
+
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 main()
