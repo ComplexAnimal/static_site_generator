cd855c7b8d080aa7448735b6fcf08184a6b3f0b1
ComplexAnimal <ElevatedAnimal@gmail.com>
2026-02-02 22:03:26 -0600

    Replaced generate_page function with generate_pages_recursive function in generate_page.py and main.py

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,24 +3,29 @@ import os
 from md_to_html import markdown_to_html_node
 from extract_title import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_pages_recursive(from_path, temp_path, dest_path):
 
-    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
+    with temp_path.open("r", encoding="utf-8") as f:
+        template = f.read()
 
-    with open(from_path) as file:
-        markdown = file.read()
+    for item in from_path.iterdir():
+        relative = item.relative_to(from_path)
+        dest = dest_path / relative
 
-    with open(template_path) as file:
-        template = file.read()
+        if item.is_file():
+            html_dest = dest.with_suffix(".html")
 
-    node = markdown_to_html_node(markdown)
-    html = node.to_html()
+            with item.open("r", encoding="utf-8") as f:
+                markdown = f.read()
 
-    title = extract_title(markdown)
+            node = markdown_to_html_node(markdown)
+            html = node.to_html()
+            title = extract_title(markdown)
+            page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+            html_dest.parent.mkdir(parents=True, exist_ok=True)
 
-    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+            with html_dest.open("w", encoding="utf-8") as f:
+                f.write(page)
 
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    f = open(dest_path, "w")
-    f.write(page)
-    f.close()
+        else:
+            generate_pages_recursive(item, temp_path, dest)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import os
+from pathlib import Path
 
 from copy_dir import copy_dir
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 def main():
     src = os.path.abspath("static")
     dst = os.path.abspath("public")
-
     copy_dir(src, dst)
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    from_path = Path("content")
+    temp_path = Path("template.html")
+    dest_path = Path("public")
+    generate_pages_recursive(from_path, temp_path, dest_path)
 
 main()
